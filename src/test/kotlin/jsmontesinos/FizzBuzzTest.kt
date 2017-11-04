a366e22f54e954f45469a33aa3b3ba7b235ac5c4
Juan Sánchez <jsmontesinos@gmail.com>
2017-11-04 19:13:02 +0100

    Progress returning Fizz for numbers divisible by 3

--- src/test/kotlin/jsmontesinos/FizzBuzzTest.kt
+++ src/test/kotlin/jsmontesinos/FizzBuzzTest.kt
@@ -9,13 +9,35 @@ class FizzBuzzTest : Spek({
     describe("FizzBuzzResolver") {
         it("should return 1 when input is 1") {
             var fizzBuzz = FizzBuzz()
+
             var result = fizzBuzz.resolve(1)
-            assertEquals(1, result)
+
+            assertEquals("1", result)
         }
+
+        it("should return Fizz when input is 3") {
+            var fizzBuzz = FizzBuzz()
+
+            var result = fizzBuzz.resolve(3)
+
+            assertEquals("Fizz", result)
+        }
+
+        it("should return Fizz when input is 6") {
+            var fizzBuzz = FizzBuzz()
+
+            var result = fizzBuzz.resolve(6)
+
+            assertEquals("Fizz", result)
+        }
+
     }
 
 })
 
 class FizzBuzz {
-    fun  resolve(input: Int): Int = 1
+    fun  resolve(input: Int): String {
+        if (input % 3 == 0) return "Fizz"
+        else return input.toString()
+    }
 }
